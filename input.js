0d2e4474bedce4f3308850cd518024763aeb2618
Ammar Shakir <shakirammar12@gmail.com>
2020-08-26 21:13:54 +0000

    added a user input to detect whichc way the user is moving the snake

--- input.js
+++ input.js
@@ -1,16 +1,25 @@
-const setupInput = () => {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
-  stdin.on('data', handleUserInput);
+  stdin.on('data', handleUserInput = (key) => {
+    if(key === '\u0003') {
+      process.exit();
+    } else if (key === 'w') {
+      conn.write("Move: up");
+    } else if (key === 'a') {
+      conn.write("Move: left");
+    } else if (key === 's') {
+      conn.write("Move: down");
+    } else if (key === 'd') {
+      conn.write("Move: right");
+    }
+  });
   return stdin;
 };
-
-const handleUserInput = (key) => {
-  if(key === '\u0003') {
-    process.exit();
-  }
-})
 
 module.exports = setupInput;
