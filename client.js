c3f4b11c71f05e08eff3d5e43932c19b99a3589a
Ammar Shakir <shakirammar12@gmail.com>
2020-08-26 20:19:44 +0000

    added moving keys hard coded

--- client.js
+++ client.js
@@ -9,12 +9,19 @@ const connect = function() {
   conn.setEncoding('utf8');
 
   conn.on('data', () => {
-    console.log("you ded cuz u idled");
+    setTimeout(() => {
+      console.log("you ded cuz u idled");
+    }, 3000)
   })
 
   conn.on('connect', (connect) => {
     console.log("Successfully connected to game server");
     conn.write("Name: AMS");
+    // conn.write("Move: up");
+    // setTimeout(x => {conn.write("Move: left")}, 1000);
+    // setTimeout(x => {conn.write("Move: left")}, 2000);
+    // setTimeout(x => {conn.write("Move: down")}, 3000);
+    // setTimeout(x => {conn.write("Move: right")}, 4000);
   })
 
   return conn;
